240b3c5bacb4e55a54da5f4189830241bcd1f063
FYMG <svinpetr18@gmail.com>
2024-09-03 17:04:49 +0300

    feat: setup firebase

---
+++ src/shared/configs/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp, initializeServerApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const serverApp = (token: string) => {
+  return initializeServerApp(firebaseConfig, {
+    authIdToken: token,
+  });
+};
+
+const auth = getAuth(app);
+
+export { app, serverApp, auth };
