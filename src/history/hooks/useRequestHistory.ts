6a4a15064ba8bb99e95bd30585bfded0347e576d
FYM <34478249+FYMG@users.noreply.github.com>
2024-09-14 22:13:47 +0300

    History (#15)
    * docs: update README.md
    
    * feat: implement history

--- src/history/hooks/useRequestHistory.ts
+++ src/history/hooks/useRequestHistory.ts
@@ -1,20 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HistoryItem } from '@history/models';
+import { app } from '@shared/configs';
+
+function getHistory(): HistoryItem[] {
+  'use client';
+
+  const history = localStorage.getItem(app.HISTORY_KEY);
+  let parsedHistory: HistoryItem[];
+
+  try {
+    parsedHistory = JSON.parse(history || '[]');
+  } catch (e) {
+    localStorage.removeItem(app.HISTORY_KEY);
+    parsedHistory = [];
+  }
+
+  if (!Array.isArray(parsedHistory)) {
+    localStorage.removeItem(app.HISTORY_KEY);
+    parsedHistory = [];
+  }
+
+  return parsedHistory;
+}
 
 const useRequestHistory = () => {
   const [history, setHistory] = useState<HistoryItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setHistory(getHistory());
+    setIsLoading(false);
+  }, []);
 
   const addHistory = (item: Omit<HistoryItem, 'sendTime'>) => {
-    setHistory([
+    const newHistory = [
       ...history,
       {
         ...item,
         sendTime: Date.now(),
       },
-    ]);
+    ];
+
+    setHistory(newHistory);
+    localStorage.setItem(app.HISTORY_KEY, JSON.stringify(newHistory));
   };
 
   return {
+    isLoading,
     history,
     addHistory,
   };
--- src/history/models/HistoryItem.ts
+++ src/history/models/HistoryItem.ts
@@ -1,6 +1,10 @@
+import { HTTP_METHOD } from 'next/dist/server/web/http';
+
+export type Methods = HTTP_METHOD | 'GRAPHQL';
+
 interface HistoryItem {
   baseUrl: string;
-  method: string;
+  method: Methods;
   sendTime: number;
   url: string;
 }
