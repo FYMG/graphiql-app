2415b0853feca6d6ce062e4c64355c7caa7eb790
FYMG <svinpetr18@gmail.com>
2024-09-15 20:25:36 +0300

    feat: add required to login password

--- src/auth/components/LoginForm/LoginForm.tsx
+++ src/auth/components/LoginForm/LoginForm.tsx
@@ -33,9 +33,13 @@ function LoginForm() {
       .email({
         message: t('invalid-email'),
       }),
-    password: z.string({
-      message: t('required'),
-    }),
+    password: z
+      .string({
+        message: t('required'),
+      })
+      .min(1, {
+        message: t('required'),
+      }),
   });
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
